6bdc644fa0b1e5720f063fdb4ead56632502cdbf
tkv00 <tkv0098@gmail.com>
2025-11-02 20:52:02 +0900

    Read.me update

---
+++ index.js
@@ -0,0 +1,83 @@
+import {writeFileSync} from 'fs';
+import Parser from 'rss-parser';
+
+let text = `# Hi,there I'm DoYeon Kim.
+> I am a developer who keeps up with new technology trends and evolving languages without fear, always eager to learn and grow. I prefer collaboration over working alone, enjoying the exchange of ideas and feedback with teammates. I strive to write clean code and build clean architectures.
+
+<br/>
+<br/>
+
+<div align="left">
+ 
+## 💭 Skills
+
+| Language | Framework |
+|---|---|
+| [![My Skills](https://go-skill-icons.vercel.app/api/icons?i=java,js,ts,c,cpp,kotlin,python&perline=7)](https://skillicons.dev) | [![My Skills](https://go-skill-icons.vercel.app/api/icons?i=spring,react&perline=6)](https://skillicons.dev) |
+
+| Database | DevOps |
+|---|---|
+| [![My Skills](https://go-skill-icons.vercel.app/api/icons?i=mysql,dbeaver,sqlite,redis&perline=6)](https://skillicons.dev) | [![My Skills](https://go-skill-icons.vercel.app/api/icons?i=jenkins,docker,githubactions&perline=6)](https://skillicons.dev) |
+
+| Cloud & Infra | Testing Tools |
+|---|---|
+| [![My Skills](https://go-skill-icons.vercel.app/api/icons?i=ec2,s3,ubuntu&perline=6)](https://skillicons.dev) | [![My Skills](https://go-skill-icons.vercel.app/api/icons?i=junit,postman,grafana&perline=6)](https://skillicons.dev) |
+
+| Communication | Security |
+|---|---|
+| [![My Skills](https://go-skill-icons.vercel.app/api/icons?i=notion,github,slack,figma,jira&perline=6)](https://skillicons.dev) | [![My Skills](https://go-skill-icons.vercel.app/api/icons?i=jwt&perline=6)](https://skillicons.dev) |
+
+
+ ## 💭 Activity
+ #### [🦁멋쟁이 사자처럼 12th 2024.03 ~ 2024.12](https://drive.google.com/file/d/1KYQ7LpR80xDTVlhYvOTVUT4AxKFBpr4L/view?usp=sharing)
+ #### [✏️SQLD 자격증 2024.11](https://drive.google.com/file/d/16L_jv_ocHgccdDgDT-IH1ni5fhaYRh99/view?usp=sharing)
+  #### ✏️정보처리기사 자격증 2025.06
+ #### [💻SWYP 8기 BE파트 2025.01 ~ 2025.03](https://drive.google.com/file/d/1HlW1rYGeyyXsAyE5MHifZTP9vYFo8Uei/view)
+ 
+
+## 💭 project
+
+<a href="https://github.com/Likelion-YeungNam-Univ/12th-BeginnerFit-FE">Bigginer Fit(2024.7.14 ~ 2024.08.07)</a> 
+
+
+<a href="https://github.com/Yeungnam-Nyang">영남냥(2024.09.08~2024.12.15)</a>
+
+<a href="https://github.com/software-project-yu/WealthTracker-BE">WealthTracker(2024.09.28~2024.12.06)</a>
+
+<a href="https://github.com/sw-security-web-app/sw-security-web-app_BE">Vero-정보보안 SW 웹/앱 개발 공모전(2025.01.26~2025.03.07)</a>
+ 
+<a href="https://github.com/Team-Qampus/Quampus_BE">Qampus(2025.01.20 ~ 2025.03.15)</a>
+
+
+## 💭 Algorithm
+[![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=tkv00)](https://solved.ac/tkv00/)
+
+
+## 💭 Recent Posts
+`;
+
+const parser = new Parser({
+    headers:{
+        Accept: 'application/rss+xml, application/xml, text/xml; q=0.9, */*; q=0.8',
+    }
+});
+
+(async () => {
+
+    //피드
+    const feed=await parser.parseURL('https://codekim3570.tistory.com/rss');
+
+
+    for(let i=0;i<5;i++){
+        const {title,link}=feed.items[i];
+        console.log(`${title}\n${link}\n`);
+        text+=`<li><a href="${link}" target='_blank'>${title}</a></li>\n`;
+    }
+
+    text+=`</ul>`;
+
+    writeFileSync('README.md',text,'utf-8',(e)=>{
+        console.log(e);
+    });
+
+})();
