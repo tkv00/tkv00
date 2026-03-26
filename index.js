a5325ff17d10d43376880651c768d75fa0b0dc7e
tkv00 <tkv0098@gmail.com>
2026-03-26 17:59:58 +0900

    update read.me

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ let text = `
  ## 💭 Activity
  #### 🦁멋쟁이 사자처럼 12th 2024.03 ~ 2024.12
  #### 💻SWYP 8기 BE파트 2025.01 ~ 2025.03
-
+ #### 💻[LG U+] URECA 3기 백엔드 2025.08 ~ 2026.03
 <br/>
 
  ## 💭 Certificate
@@ -49,17 +49,38 @@ let text = `
 
 ## 💭 project
 
-<a href="https://github.com/Likelion-YeungNam-Univ/12th-BeginnerFit-FE">Bigginer Fit(2024.7.14 ~ 2024.08.07)</a> 
+<a href="https://github.com/Likelion-YeungNam-Univ/12th-BeginnerFit-FE"><b>Beginner Fit</b></a> <sub>(2024.07.14 ~ 2024.08.07)</sub><br/>
+운동 초보자를 위한 맞춤형 운동 루틴 추천 및 기록 서비스
 
+<br/>
 
-<a href="https://github.com/Yeungnam-Nyang">영남냥(2024.09.08~2024.12.15)</a>
+<a href="https://github.com/Yeungnam-Nyang"><b>영남냥</b></a> <sub>(2024.09.08 ~ 2024.12.15)</sub><br/>
+교내 길고양이 제보, 위치 공유, 돌봄 정보를 제공하는 서비스
 
-<a href="https://github.com/software-project-yu/WealthTracker-BE">WealthTracker(2024.09.28~2024.12.06)</a>
+<br/>
 
-<a href="https://github.com/sw-security-web-app/sw-security-web-app_BE">Vero-정보보안 SW 웹/앱 개발 공모전(2025.01.26~2025.03.07)</a>
- 
-<a href="https://github.com/Team-Qampus/Quampus_BE">Qampus(2025.01.20 ~ 2025.03.15)</a>
+<a href="https://github.com/software-project-yu/WealthTracker-BE"><b>WealthTracker</b></a> <sub>(2024.09.28 ~ 2024.12.06)</sub><br/>
+가계부 데이터 기반 소비 분석과 맞춤형 피드백을 제공하는 자산 관리 서비스
 
+<br/>
+
+<a href="https://github.com/sw-security-web-app/sw-security-web-app_BE"><b>Vero</b></a> <sub>(2025.01.26 ~ 2025.03.07)</sub><br/>
+정보보안 SW 웹/앱 개발 공모전을 위해 진행한 보안 중심 웹 서비스 프로젝트
+
+<br/>
+
+<a href="https://github.com/Team-Qampus/Quampus_BE"><b>Qampus</b></a> <sub>(2025.01.20 ~ 2025.03.15)</sub><br/>
+대학생 대상 질문·정보 공유 기반 캠퍼스 커뮤니티 서비스
+
+<br/>
+
+<a href="https://github.com/Team-TR1L"><b>TR1L</b></a> <sub>(2026.01.07 ~ 2026.01.27)</sub> 🏆 <b>URECA 3기 종합 프로젝트 우수상</b><br/>
+대규모 청구·정산 데이터를 기반으로 고객별 청구서 생성과 발송을 안정적으로 처리하는 통신 정산 플랫폼
+
+<br/>
+
+<a href="https://github.com/one-year-gap"><b>Holliverse</b></a> <sub>(2026.02.04 ~ 2026.03.20)</sub> 🏆 <b>URECA 3기 최종융합 프로젝트 우수상</b><br/>
+고객 이탈률 예측, 추천 기능, 상담 데이터 분석을 통해 데이터 기반 운영 의사결정을 지원하는 CRM 서비스
 <br/>
 
 ## 💭 Algorithm
